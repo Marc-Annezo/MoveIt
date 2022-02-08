24562432d9a7deb9a1b00f43c39da9025b34fcb6
lderville <91890830+lderville@users.noreply.github.com>
2022-02-08 12:14:38 +0100

    Make:auth make:entity Make:form ( Participant )

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,13 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/login', name: 'login')]
-    public function login(): Response
-    {
-        return $this->render('security/login.html.twig'
 
-        );
-    }
 
     #[Route('/home', name: 'home')]
     public function accueil(): Response
