77a84c306c2f965f85710573cb6fc8449a3dcc5c
Marc Annezo <mannezo91@gmail.com>
2022-02-14 09:13:27 +0100

    ajoutlieu

--- src/Form/AjoutLieuType.php
+++ src/Form/AjoutLieuType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Lieu;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,53 @@ class AjoutLieuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('rue')
-            ->add('latitude')
-            ->add('longitude')
-            ->add('idVille')
+            ->add('nom', TextType::class, [
+                'label_attr'=> ['class'=> ' text-primary'],
+                'attr' => ['class'=>'input is-link is-rounded',
+                    'placeholder'=>'Que voulez vous faire ?',
+                    'type'=>'text',
+                    'aria-describedby'=>"inputGroup-sizing-sm"]
+                ]
+            )
+
+            ->add('rue',TextType::class, [
+                    'label_attr'=> ['class'=> ' text-primary'],
+                    'attr' => ['class'=>'input is-link is-rounded',
+                        'placeholder'=>'Où ça ?',
+                        'type'=>'text',
+                        'aria-describedby'=>"inputGroup-sizing-sm"]
+                ]
+            )
+
+            ->add('latitude',NumberType::class, [
+                    'label_attr'=> ['class'=> ' text-primary'],
+                    'attr' => ['class'=>'input is-link is-rounded',
+                        'placeholder'=>'',
+                        'type'=>'text',
+                        'aria-describedby'=>"inputGroup-sizing-sm"]
+                ])
+
+            ->add('longitude', NumberType::class, [
+                    'label_attr'=> ['class'=> ' text-primary'],
+                    'attr' => ['class'=>'input is-link is-rounded',
+                        'placeholder'=>'',
+                        'type'=>'text',
+                        'aria-describedby'=>"inputGroup-sizing-sm"]
+            ])
+
+            ->add('idVille', TextType::class, [
+                'label' => 'Ville',
+                'label_attr'=> ['class'=> ' text-primary'],
+                'attr' => ['class'=>'input is-link is-rounded',
+                    'placeholder'=>'',
+                    'type'=>'date',
+                    'aria-describedby'=>"inputGroup-sizing-sm"]
+            ])
+
+            ->add('valider', SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => ['class' => 'button is-block is-info is-normal '],
+            ])
         ;
     }
 
